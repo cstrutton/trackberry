8f87f61f1bc4e6e456ae7f73341676629ce02dfc
Chris Strutton <chris@rodandfly.ca>
2022-04-25 11:40:41 -0400

    Supervisor menu boilerplate

--- supervisor_menu/models.py
+++ supervisor_menu/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class SupervisorMenuItem(models.Model):
+
+    text = models.CharField(max_length=20, help_text='Menu Item Text')
+    url = models.CharField(max_length=40)
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
